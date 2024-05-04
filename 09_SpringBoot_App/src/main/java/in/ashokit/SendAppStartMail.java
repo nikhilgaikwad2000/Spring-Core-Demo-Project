b79b1504b7abbc996c4c590714c2075f7ef11380
Nikhil Gaikwad <122869789+nikhilgaikwad2000@users.noreply.github.com>
2024-05-04 19:28:57 +0530

    Add files via upload

---
+++ 09_SpringBoot_App/src/main/java/in/ashokit/SendAppStartMail.java
@@ -0,0 +1,15 @@
+package in.ashokit;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendAppStartMail implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("logic excuting the Send mail......");
+	}
+
+}
